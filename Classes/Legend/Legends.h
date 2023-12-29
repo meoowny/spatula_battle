aae8acb5be7eaad42ac8828dc944299457f70766
Triumph31 <triumphkai31@gmail.com>
2023-12-29 22:29:28 +0800

    new(Resources* ):卡牌图片更新（全部英雄）

--- Classes/Legend/Legends.h
+++ Classes/Legend/Legends.h
@@ -7,19 +7,13 @@ class Aphelios : public Legend
 public:
 	Aphelios(LegendInfo* const info)
 		: Legend(info) { }
-	//Aphelios() :Legend("Aphelios", 580, 2)
-	//{
-	//	_attack_damage = 70;
-	//	_armor = 26;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	// 由于英雄的信息指针只由玩家信息保管，因此不做特殊处理，生成的指针统一由 PlayerInfo 销毁
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 30, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -38,18 +32,12 @@ class Caitlyn :public Legend
 public:
 	Caitlyn(LegendInfo* const info)
 		: Legend(info) { }
-	//Caitlyn() :Legend("Caitlyn", 480, 4)
-	//{
-	//	_attack_damage = 62;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 5;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Caitlyn", 480, 4, 62, 28, 30, 5);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -68,18 +56,12 @@ class Ezreal :public Legend
 public:
 	Ezreal(LegendInfo* const info)
 		: Legend(info) { }
-	//Ezreal() :Legend("Ezreal", 600, 2)
-	//{
-	//	_attack_damage = 60;
-	//	_armor = 24;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Ezreal", 600, 2, 60, 24, 30, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -96,18 +78,12 @@ class Lucian :public Legend
 public:
 	Lucian(LegendInfo* const info)
 		: Legend(info) { }
-	//Lucian() :Legend("Lucian", 641, 2)
-	//{
-	//	_attack_damage = 60;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 3;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Lucian", 641, 2, 60, 28, 30, 3);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -126,18 +102,12 @@ class Miss_Fortune :public Legend
 public:
 	Miss_Fortune(LegendInfo* const info)
 		: Legend(info) { }
-	//Miss_Fortune() :Legend("Miss_Fortune", 640, 2)
-	//{
-	//	_attack_damage = 52;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Miss_Fortune", 640, 2, 52, 28, 30, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -156,18 +126,12 @@ class Graves :public Legend
 public:
 	Graves(LegendInfo* const info)
 		: Legend(info) { }
-	//Graves() :Legend("Graves", 625, 2)
-	//{
-	//	_attack_damage = 68;
-	//	_armor = 33;
-	//	_critical_strike_chance = 10;
-	//	_range = 3;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Graves", 625, 2, 68, 33, 50, 3);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -186,18 +150,12 @@ class Varus :public Legend
 public:
 	Varus(LegendInfo* const info)
 		: Legend(info) { }
-	//Varus() :Legend("Varus", 600, 2)
-	//{
-	//	_attack_damage = 62;
-	//	_armor = 27;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Varus", 600, 2, 62, 27, 30, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -218,18 +176,12 @@ class Katarina :public Legend
 public:
 	Katarina(LegendInfo* const info)
 		: Legend(info) { }
-	//Katarina() :Legend("Katarina", 672, 2)
-	//{
-	//	_attack_damage = 58;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Katarina", 672, 2, 58, 28, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -248,18 +200,12 @@ class Akali :public Legend
 public:
 	Akali(LegendInfo* const info)
 		: Legend(info) { }
-	//Akali() :Legend("Akali", 570, 2)
-	//{
-	//	_attack_damage = 62;
-	//	_armor = 23;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Akali", 570, 2, 62, 23, 10, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -278,18 +224,12 @@ class Annie :public Legend
 public:
 	Annie(LegendInfo* const info)
 		: Legend(info) { }
-	//Annie() :Legend("Annie", 594, 2)
-	//{
-	//	_attack_damage = 50;
-	//	_armor = 19;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Annie", 594, 2, 50, 19, 10, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -307,18 +247,12 @@ class Viktor :public Legend
 public:
 	Viktor(LegendInfo* const info)
 		: Legend(info) { }
-	//Viktor() :Legend("Viktor", 600, 2)
-	//{
-	//	_attack_damage = 53;
-	//	_armor = 23;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Aphelios", 600, 2, 53, 23, 10, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -336,18 +270,12 @@ class Sona :public Legend
 public:
 	Sona(LegendInfo* const info)
 		: Legend(info) { }
-	//Sona() :Legend("Sona", 550, 2)
-	//{
-	//	_attack_damage = 49;
-	//	_armor = 26;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Sona", 550, 2, 49, 26, 10, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -365,18 +293,12 @@ class Jhin :public Legend
 public:
 	Jhin(LegendInfo* const info)
 		: Legend(info) { }
-	//Jhin() :Legend("Jhin", 655, 2)
-	//{
-	//	_attack_damage = 70;
-	//	_armor = 24;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Jhin", 655, 2, 70, 24, 40, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -396,18 +318,12 @@ class Viego :public Legend
 public:
 	Viego(LegendInfo* const info)
 		: Legend(info) { }
-	//Viego() :Legend("Viego", 630, 2)
-	//{
-	//	_attack_damage = 70;
-	//	_armor = 34;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Viego", 630, 2, 70, 34, 70, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -425,18 +341,12 @@ class Qiyana :public Legend
 public:
 	Qiyana(LegendInfo* const info)
 		: Legend(info) { }
-	//Qiyana() :Legend("Qiyana", 590, 2)
-	//{
-	//	_attack_damage = 66;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Qiyana", 590, 2, 66, 28, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -454,18 +364,12 @@ class Kayn :public Legend
 public:
 	Kayn(LegendInfo* const info)
 		: Legend(info) { }
-	//Kayn() :Legend("Kayn", 600, 2)
-	//{
-	//	_attack_damage = 70;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Kayn", 600, 2, 70, 28, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -483,18 +387,12 @@ class Bel_Veth :public Legend
 public:
 	Bel_Veth(LegendInfo* const info)
 		: Legend(info) { }
-	//Bel_Veth() :Legend("Bel_Veth", 610, 2)
-	//{
-	//	_attack_damage = 60;
-	//	_armor = 32;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Bel_Veth", 610, 2, 60, 32, 20, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -512,18 +410,12 @@ class Ekko :public Legend
 public:
 	Ekko(LegendInfo* const info)
 		: Legend(info) { }
-	//Ekko() :Legend("Ekko", 655, 2)
-	//{
-	//	_attack_damage = 58;
-	//	_armor = 32;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Ekko", 655, 2, 58, 32, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -543,18 +435,12 @@ class Riven : public Legend
 public:
 	Riven(LegendInfo* const info)
 		: Legend(info) { }
-	//Riven() :Legend("Riven", 630, 2)
-	//{
-	//	_attack_damage = 64;
-	//	_armor = 33;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Riven", 630, 2, 64, 33, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -572,18 +458,11 @@ class Fiora :public Legend
 public:
 	Fiora(LegendInfo* const info)
 		: Legend(info) { }
-	//Fiora() :Legend("Fiora", 620, 2)
-	//{
-	//	_attack_damage = 68;
-	//	_armor = 33;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Fiora", 620, 2, 68, 33, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -601,18 +480,11 @@ class Camille :public Legend
 public:
 	Camille(LegendInfo* const info)
 		: Legend(info) { }
-	//Camille() :Legend("Camille", 646, 2)
-	//{
-	//	_attack_damage = 68;
-	//	_armor = 33;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Camille", 646, 2, 68, 33, 5, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -630,18 +502,11 @@ class Irelia :public Legend
 public:
 	Irelia(LegendInfo* const info)
 		: Legend(info) { }
-	//Irelia() :Legend("Irelia", 590, 2)
-	//{
-	//	_attack_damage = 65;
-	//	_armor = 36;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Irelia", 590, 2, 65, 36, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -658,18 +523,11 @@ class Sett :public Legend
 public:
 	Sett(LegendInfo* const info)
 		: Legend(info) { }
-	//Sett() :Legend("Sett", 670, 2)
-	//{
-	//	_attack_damage = 60;
-	//	_armor = 33;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Aphelios", 670, 2, 60, 33, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -698,7 +556,7 @@ public:
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Ganplank", 600, 2, 64, 31, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -718,18 +576,12 @@ class Thresh :public Legend
 public:
 	Thresh(LegendInfo* const info)
 		: Legend(info) { }
-	//Thresh() :Legend("Thresh", 1000, 2)
-	//{
-	//	_attack_damage = 56;
-	//	_armor = 31;
-	//	_critical_strike_chance = 10;
-	//	_range = 3;
-	//}
+	
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Thresh", 1000, 2, 10, 31, 0, 3);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -747,18 +599,11 @@ class Renata_Glasc :public Legend
 public:
 	Renata_Glasc(LegendInfo* const info)
 		: Legend(info) { }
-	//Renata_Glasc() :Legend("Renata_Glasc", 500, 2)
-	//{
-	//	_attack_damage = 56;
-	//	_armor = 28;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Renta_Glasc", 700, 2, 10, 28, 20, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -776,18 +621,11 @@ class Rakan :public Legend
 public:
 	Rakan(LegendInfo* const info)
 		: Legend(info) { }
-	//Rakan() :Legend("Rakan", 1000, 2)
-	//{
-	//	_attack_damage = 62;
-	//	_armor = 32;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Rakan", 1000, 2, 10, 32, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -805,18 +643,11 @@ class Bard :public Legend
 public:
 	Bard(LegendInfo* const info)
 		: Legend(info) { }
-	//Bard() :Legend("Bard", 1200, 2)
-	//{
-	//	_attack_damage = 52;
-	//	_armor = 34;
-	//	_critical_strike_chance = 10;
-	//	_range = 4;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Bard", 1200, 2, 10, 34, 10, 4);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -834,18 +665,11 @@ class Nautilus :public Legend
 public:
 	Nautilus(LegendInfo* const info)
 		: Legend(info) { }
-	//Nautilus() :Legend("Nautilus", 1600, 2)
-	//{
-	//	_attack_damage = 50;
-	//	_armor = 39;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Nautilus", 1600, 2, 5, 39, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
@@ -863,18 +687,11 @@ class Pyke :public Legend
 public:
 	Pyke(LegendInfo* const info)
 		: Legend(info) { }
-	//Pyke() :Legend("Pyke", 900, 2)
-	//{
-	//	_attack_damage = 70;
-	//	_armor = 45;
-	//	_critical_strike_chance = 10;
-	//	_range = 1;
-	//}
 	int abilityAttack();
 
 	static LegendInfo* createInfo()
 	{
-		LegendInfo* info = new LegendInfo("Aphelios", 580, 2, 70, 26, 10, 4);
+		LegendInfo* info = new LegendInfo("Pyke", 900, 2, 60, 45, 10, 1);
 		if (info == NULL) {
 			throw "Error";
 		}
